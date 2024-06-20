a58e0cd7a695d9b4f3018a7a82160c6429c7176a
Jallouli-Yassine <jungloul1@gmail.com>
2024-06-20 01:51:03 +0100

    first commit

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+const mobileBtn = document.querySelector("#mobile-btn")
+const mobileMenu = document.querySelector("#mobile-menu")
+
+mobileBtn.addEventListener('click', () => mobileMenu.classList.toggle('hidden'))
